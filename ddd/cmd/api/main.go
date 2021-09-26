b81ba155632edca8a13f3039f2ab7793d69d7f05
alochym <alochym@gmail.com>
2021-09-26 14:30:51 +0700

    add storage sqlite

--- ddd/cmd/api/main.go
+++ ddd/cmd/api/main.go
@@ -1,10 +1,71 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
+	"fmt"
+
+	"github.com/alochym01/thecodecamp_1/domain/users"
 	"github.com/alochym01/thecodecamp_1/router"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	app := router.Router()
-	app.Run()
+	// Command-line Flags
+	addr := flag.String("addr", ":8080", "HTTP network address")
+
+	// Importantly, we use the flag.Parse() function to parse the command-line
+	// This reads in the command-line flag value and assigns it to the addr
+	flag.Parse()
+
+	db := SetupSqliteConn("foo.db")
+	defer db.Close()
+
+	app := router.Router(db)
+	app.Run(*addr)
+}
+
+func SetupSqliteConn(f string) *sql.DB {
+	dbConn, err := sql.Open("sqlite3", f)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return dbConn
+}
+
+func mockDB(db *sql.DB) {
+	// ID       string `json:"id"`
+	// Email    string `json:"email"`
+	// Password string `json:"password"`
+	// Role     string `json:"role"`
+	// Status   string `json:"status"`
+	sqltable := `CREATE TABLE users (id CHARACTER (32) NOT NULL PRIMARY KEY, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, status VARCHAR(255) NOT NULL)`
+
+	// Execute SQL Statements
+	_, err := db.Exec(sqltable)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	var temp = []users.User{
+		{ID: "1000", Email: "hadn4@fpt.com.vn", Password: "Alochym@123", Role: "Husband", Status: "1"},
+		{ID: "1001", Email: "1nhuntt@fpt.com.vn", Password: "Alochym@123", Role: "Wife", Status: "1"},
+		{ID: "1002", Email: "2nhuntt@fpt.com.vn", Password: "Alochym@123", Role: "Wife", Status: "0"},
+		{ID: "1003", Email: "3nhuntt@fpt.com.vn", Password: "Alochym@123", Role: "Wife", Status: "1"},
+		{ID: "1004", Email: "4nhuntt@fpt.com.vn", Password: "Alochym@123", Role: "Wife", Status: "0"},
+		{ID: "1005", Email: "5nhuntt@fpt.com.vn", Password: "Alochym@123", Role: "Wife", Status: "1"},
+		{ID: "1006", Email: "6nhuntt@fpt.com.vn", Password: "Alochym@123", Role: "Wife", Status: "1"},
+		{ID: "1007", Email: "7nhuntt@fpt.com.vn", Password: "Alochym@123", Role: "Wife", Status: "10"},
+		{ID: "1008", Email: "8nhuntt@fpt.com.vn", Password: "Alochym@123", Role: "Wife", Status: "1"},
+		{ID: "1009", Email: "9nhuntt@fpt.com.vn", Password: "Alochym@123", Role: "Wife", Status: "1"},
+	}
+	for i := range temp {
+		sqlstmt := `INSERT INTO users(id, email, password, role, status) VALUES(?, ?, ?, ?, ?)`
+		_, err := db.Exec(sqlstmt, temp[i].ID, temp[i].Email, temp[i].Password, temp[i].Role, temp[i].Status)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+	}
 }
--- ddd/storage/sqlite/user.go
+++ ddd/storage/sqlite/user.go
@@ -2,6 +2,7 @@ package sqlite
 
 import (
 	"database/sql"
+	"fmt"
 
 	"github.com/alochym01/thecodecamp_1/domain/users"
 )
@@ -20,17 +21,55 @@ func NewRepository(db *sql.DB) users.Repository {
 
 // FindAll ...
 func (u Repository) FindAll() ([]users.User, error) {
-	// result, err := u.db.Query("select * from users")
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// fmt.Println(result)
-	result1 := []users.User{}
-	return result1, nil
+	sqlstmt := "select * from users"
+
+	rows, err := u.db.Query(sqlstmt)
+
+	// check err from server DB and Query DB
+	if err != nil {
+		fmt.Println("Server Err", err.Error())
+		// return nil, errs.NewServerError(err.Error())
+	}
+
+	temp := []users.User{}
+	// users := make([]users.User, 0)
+
+	for rows.Next() {
+		c := users.User{}
+		err := rows.Scan(&c.ID, &c.Email, &c.Password, &c.Role, &c.Status)
+		// check err from server DB and Scan function
+		if err != nil {
+			if err == sql.ErrNoRows {
+				fmt.Println("Not Found", err.Error())
+				// return nil, errs.NewNotFoundError(sql.ErrNoRows.Error())
+				return nil, err
+			}
+			fmt.Println("Server Err", err.Error())
+			// return nil, errs.NewServerError("Server Error")
+			return nil, err
+		}
+		temp = append(temp, c)
+	}
+	return temp, nil
+
 }
 
 // ByEmail ...
 func (u Repository) ByEmail(email string) (*users.User, error) {
-	// user, err := u.db.QueryRow("select * from users where email=?", email)
-	return &users.User{}, nil
+	sqlstmt := "select * from users where email=?"
+	row := u.db.QueryRow(sqlstmt, email)
+	c := users.User{}
+	err := row.Scan(&c.ID, &c.Email, &c.Password, &c.Role, &c.Status)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("Not Found", err.Error())
+			return nil, err
+			// return nil, errs.NewNotFoundError(sql.ErrNoRows.Error())
+		}
+		fmt.Println("Server Err", err.Error())
+		return nil, err
+		// return nil, errs.NewServerError(err.Error())
+	}
+
+	return &c, nil
 }
